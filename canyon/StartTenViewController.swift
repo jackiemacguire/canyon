bd8470b9338e59f76835500e8248f52ad2569ed4
janiechang <janiechang0425@gmail.com>
2022-06-15 16:00:13 -0400

    coded in StartTenViewController

--- canyon/StartTenViewController.swift
+++ canyon/StartTenViewController.swift
@@ -9,13 +9,20 @@ import UIKit
 
 class StartTenViewController: UIViewController {
 
+    @IBOutlet weak var nameOfPose: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func buttonTapped(_ sender: UIButton) {
+    let yogaPoses = [ "Downward Dog","Table Top","Child's Pose","Warrior I","Warrior II","Warrior III","Reverse Warrior","Garland Pose","Cat-Cow","Cobra Pose","Half Spinal Twist","Head To Knee Pose","Happy Baby Pose","Chair Pose","Lizard Pose"]
+        
+    nameOfPose.text = yogaPoses.randomElement()
+    }
+    
     /*
     // MARK: - Navigation
 
