4b6bcaba787697b170ca96d60a16b515d8ab97f8
Gabriela De Melo <gabrielammelo1@gmail.com>
2022-06-15 18:24:51 -0300

    quotes

--- canyon/ViewController.swift
+++ canyon/ViewController.swift
@@ -9,11 +9,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var quote: UILabel!
+    
+
+    //func show if UIViewController = open {
+        //quote.random
+         
+    @IBAction func wisdom(_ sender: Any) {
+        let options = ["Quiet the mind and the soul will speak", "Self love is the greatest medicine",
+        "Become a priority in  your life."
+        ,
+        "Embrace and love your body. It is the most amazing thing you’ll ever own",
+        "It’s not about being the best. It’s about being better than you were yesterday." ,"Self-care is how you take your power back. - Lalah Delia", "Our bodies are our gardens – our wills are our gardeners. – William Shakespeare"]
+        quote.text = options.randomElement()
+        
+    }
+
+
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
+       super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
-}
 
+
+
+}
